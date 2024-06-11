c6a4f648865bc65a1c741bb6170d32b221003c43
anas20023 <yorder07@gmail.com>
2024-06-11 13:47:21 +0600

    chore: Add nodemon for automatic server restarts

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-require("dotenv").config();
-
+require('dotenv').config()
+//console.log(process.env.PORT)
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Server Working");
+  res.send("Server Working ");
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
