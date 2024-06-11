9b506ead38b45658d90983c8fccb85c5bc81d1ae
anas20023 <yorder07@gmail.com>
2024-06-11 14:25:35 +0600

    chore: Update route handler to use app.use instead of app.get

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use("/", (req, res) => {
-  res.send("Server Running");
+  res.json({"Messege":"Server Working !!"});
 });
 app.listen(port, () => {
   console.log(`Server is Running in port ${port}`);
