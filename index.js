49d317d3d8ee0961f2af8e3542a0d26c6a35a54c
anas20023 <yorder07@gmail.com>
2024-06-11 14:20:01 +0600

    chore: Update .gitignore and package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get("/",(req,res)=>{
+
+    res.send("Server Running");
+})
+app.listen(port, () => {
+  console.log(`Server is Running in port ${port}`);
+});
